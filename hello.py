2c7adee19d09ab01197bcb1c1dc5db0a02b6a5eb
alexey logunov <al_logunov@mail.ru>
2021-09-13 11:31:33 +0300

    create file gunicorn.conf, hello.py and modified init.sh

--- hello.py
+++ hello.py
@@ -4,3 +4,10 @@ def add(environ, start_response):
     body = "\n".join(environ.get('QUERY_STRING').split("&"))
     start_response(status, headers)
     return iter([body.encode('utf-8')])
+
+
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
