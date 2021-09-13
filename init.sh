14a828b27c5c819fa5688725395d4067a40c0a05
alexey-logunov <64679502+alexey-logunov@users.noreply.github.com>
2021-09-13 16:41:33 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo rm -rf /etc/nginx/sites-available/default
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
