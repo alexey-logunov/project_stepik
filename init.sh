04fc54328e650bbf6c8795020419cbf118182def
alexey-logunov <al_logunov@mail.ru>
2021-09-16 14:02:21 +0300

    modufied settings.py init.sh

--- init.sh
+++ init.sh
@@ -2,4 +2,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
+sudo ln -sf /home/box/web/etc/ask_conf.py  /etc/gunicorn.d/ask_conf.py
+sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
 sudo /etc/init.d/gunicorn restart
