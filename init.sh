c8203f00681f8aadf8b685efa9e6dd9a2c016f86
alexey-logunov <al_logunov@mail.ru>
2021-09-18 20:09:02 +0300

    modified init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf  /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/ask_conf.py  /etc/gunicorn.d/ask_conf.py
-sudo gunicorn -c /etc/gunicorn.d/ask_conf.py ask.wsgi:application
-sudo /etc/init.d/gunicorn restart
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:add
+sudo gunicorn -c /home/box/web/etc/ask_conf.py wsgi:application
