31b1b05386a22d832eea9f417e8c10c711aa059d
alexey-logunov <64679502+alexey-logunov@users.noreply.github.com>
2021-09-20 15:41:21 +0300

    Update ask_conf.py

--- etc/ask_conf.py
+++ etc/ask_conf.py
@@ -1,12 +1,12 @@
 CONFIG = {
-'mode': 'django',
+'mode': 'wsgi',
 'working_dir': '/home/box/web/ask',
 'python': '/usr/bin/python3',
     'args': (
     '--bind=0.0.0.0:8000',
     '--workers=2',
     '--timeout=10',
-    'ask.wsgi'
+    'ask.wsgi:application'
     )
 }
 
