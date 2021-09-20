5893b48cc516ac0581e03a28f296c1340e95aba3
alexey-logunov <64679502+alexey-logunov@users.noreply.github.com>
2021-09-20 14:26:47 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
-sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.py  /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/ask_conf.py /etc/gunicorn.d/ask_conf.py
+sudo /etc/init.d/gunicorn restart
 #sudo gunicorn -c /home/alexey-logunov/PycharmProjects/web/etc/gunicorn.py hello:add
 #sudo gunicorn -c /home/box/web/etc/ask_conf.py ask.wsgi:application
 
