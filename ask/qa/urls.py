380c060194d11b6bee1ec2d5a59b6218884a0092
alexey-logunov <64679502+alexey-logunov@users.noreply.github.com>
2021-09-20 12:03:32 +0300

    Update urls.py

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -26,3 +26,16 @@ urlpatterns = [
     url(r'^popular/', views.test, name='popular'),
     url(r'^new/', views.test, name='new')
 ]
+
+# from django.urls import path
+# from . import views
+#
+# urlpatterns = [
+#     path('', views.test, name='index'),
+#     path('login/', views.test, name='login'),
+#     path('signup/', views.test, name='signup'),
+#     path('question/<int:id>/', views.test, name='question'),
+#     path('ask/', views.test, name='ask'),
+#     path('popular/', views.test, name='popular'),
+#     path('new/', views.test, name='new')
+# ]
