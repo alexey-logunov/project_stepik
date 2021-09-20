e25ed9611bfb5c76346c52011b410ffbd0ea0d5e
alexey-logunov <64679502+alexey-logunov@users.noreply.github.com>
2021-09-20 10:59:29 +0300

    Update ask_conf.py

--- etc/ask_conf.py
+++ etc/ask_conf.py
@@ -1,14 +1,17 @@
-CONFIG = {
-'mode': 'wsgi',
-'working_dir': '/home/box/web/ask',
-'python': '/usr/bin/python3',
-    'args': (
-    '--bind=0.0.0.0:8000',
-    '--workers=2',
-    '--timeout=10',
-    'wsgi:application'
-    )
-}
+pythonpath = '/home/box/web/ask'
+bind = '0.0.0.0:8000'
+
+# CONFIG = {
+# 'mode': 'wsgi',
+# 'working_dir': '/home/box/web/ask',
+# 'python': '/usr/bin/python3',
+#     'args': (
+#     '--bind=0.0.0.0:8000',
+#     '--workers=2',
+#     '--timeout=10',
+#     'wsgi:application'
+#     )
+# }
 
 # CONFIG = {
 # 'mode': 'wsgi',
