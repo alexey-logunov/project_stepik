9976e8eb1025f76b185701ba2671ea2e1c796bc5
alexey-logunov <al_logunov@mail.ru>
2021-10-06 23:46:12 +0300

    test OK

--- ask/qa/admin.py
+++ ask/qa/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Question
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'added_at', 'rating', 'author', 'likes')
+    list_display = ('id', 'title', 'added_at', 'rating', 'author')
     list_display_links = ('id', 'title')
     search_fields = ('title', 'text')
     list_editable = ('author',)
