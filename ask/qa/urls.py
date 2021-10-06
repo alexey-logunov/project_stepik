00f58b77e7c487ebdd2d60bea9d09157f1f314da
alexey-logunov <al_logunov@mail.ru>
2021-10-06 19:02:13 +0300

    created base.html, index.html, popular.html

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^', views.MainView.as_view(), name='index'),
+    url(r'^', views.MainView.as_view(), name='home'),
     url(r'^login/', views.test, name='login'),
     url(r'^signup/', views.test, name='signup'),
     url(r'^question/<int:pk>/', views.QuestionView.as_view(), name='question_details'),
