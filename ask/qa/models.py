b06e5d5100d6e821418df34b3eb4d84663917cf5
alexey-logunov <al_logunov@mail.ru>
2021-10-07 21:38:59 +0300

    modified models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -15,10 +15,10 @@ class Question(models.Model):
     objects = QuestionManager()
     title = models.CharField(max_length=255, blank=True)
     text = models.TextField(blank=True)
-    added_at = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateTimeField(auto_now_add=True, blank=True)
     rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    likes = models.ManyToManyField(User, related_name='likes_set')
+    likes = models.ManyToManyField(User, related_name='likes_set', null=True)
 
     def get_absolute_url(self):
         return reverse('question', kwargs={'question_id': self.pk})
@@ -34,7 +34,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField(blank=True)
-    added_at = models.DateTimeField(auto_now_add=True)
+    added_at = models.DateTimeField(auto_now_add=True, blank=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True, related_name='answers')
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
