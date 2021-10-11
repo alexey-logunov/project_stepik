1913de12611c3928ad6fa6b92b3562dc1b0b4ae5
alexey-logunov <al_logunov@mail.ru>
2021-10-11 20:22:06 +0300

    setting up admin

--- ask/qa/admin.py
+++ ask/qa/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Question
+from django.utils.safestring import mark_safe
+
 
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'added_at', 'rating', 'author')
@@ -7,6 +9,20 @@ class QuestionAdmin(admin.ModelAdmin):
     search_fields = ('title', 'text')
     list_editable = ('author',)
     list_filter = ('author', 'title')
+    fields = ('title', 'text', 'added_at', 'rating', 'author', 'likes')
+    readonly_fields = ('added_at', 'likes')
+    save_on_top = True
+
+    # def get_photo(self, obj):
+    #     if obj.photo:
+    #         return mark_safe(f'<img src="{obj.photo.url}" width="75">')
+    #     else:
+    #         return 'Фото не добавлено'
+    #
+    # get_photo.short_description = 'Фото'
 
 
 admin.site.register(Question, QuestionAdmin)
+
+admin.site.site_title = 'Управление вопросами'
+admin.site.site_header = 'Управление вопросами'
